89b962965d71e47253548e437f6d3f2e8654bf13
Florian Hatat <pykol@hatat.me>
2020-05-16 14:31:21 +0200

    Facétie, je préfère les tabulations

--- inscrire/views/candidats.py
+++ inscrire/views/candidats.py
@@ -10,67 +10,67 @@ from inscrire.models.fiches import Fiche
 USER = get_user_model()
 
 def set_candidat(_dispatch):
-    """décorateur de dispatch; vérifie que l'utilisateur est un candidat et renseigne self.candidat"""
+	"""décorateur de dispatch; vérifie que l'utilisateur est un candidat et renseigne self.candidat"""
 
-    def dispatch(self, request, *args, **kwargs):
-        user = request.user
-        try:
-            candidat = user.candidat
-            self.candidat = candidat
-        except USER.candidat.RelatedObjectDoesNotExist:
-            return redirect("/")
-        return _dispatch(self, request,*args, **kwargs)
-    return dispatch
+	def dispatch(self, request, *args, **kwargs):
+		user = request.user
+		try:
+			candidat = user.candidat
+			self.candidat = candidat
+		except USER.candidat.RelatedObjectDoesNotExist:
+			return redirect("/")
+		return _dispatch(self, request,*args, **kwargs)
+	return dispatch
 
 
 @method_decorator(login_required, name='dispatch')
 class CandidatDetail(TemplateView):
-    """Affiche les informations personnelles d'un candidat"""
+	"""Affiche les informations personnelles d'un candidat"""
 
-    template_name = "candidat.html"
+	template_name = "candidat.html"
 
-    @set_candidat
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
+	@set_candidat
+	def dispatch(self, request, *args, **kwargs):
+		return super().dispatch(request, *args, **kwargs)
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context["candidat"] = self.candidat
-        context["nombre_responsables"] = self.candidat.responsables.count()
-        return context
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		context["candidat"] = self.candidat
+		context["nombre_responsables"] = self.candidat.responsables.count()
+		return context
 
 
 @method_decorator(login_required, name='dispatch')
 class ResponsableLegal(DetailView):
-    """Affiche les informations personnelles d'un responsable légal"""
+	"""Affiche les informations personnelles d'un responsable légal"""
 
-    model = ResponsableLegal
-    template_name = "responsablelegal.html"
-    context_object_name = "responsablelegal"
+	model = ResponsableLegal
+	template_name = "responsablelegal.html"
+	context_object_name = "responsablelegal"
 
-    @set_candidat
-    def dispatch(self, request, *args, **kwargs):
-        """Vérifie que le responsable à afficher est lié au candidat connecté"""
-        pk_responsables = self.candidat.responsables.values_list("pk", flat = True)
-        if not kwargs["pk"] in pk_responsables:
-            return redirect("/")
-        return super().dispatch(request, *args, **kwargs)
+	@set_candidat
+	def dispatch(self, request, *args, **kwargs):
+		"""Vérifie que le responsable à afficher est lié au candidat connecté"""
+		pk_responsables = self.candidat.responsables.values_list("pk", flat = True)
+		if not kwargs["pk"] in pk_responsables:
+			return redirect("/")
+		return super().dispatch(request, *args, **kwargs)
 
 
 class CandidatUpdate(UpdateView):
-    """Permet la modification des informations personnelles"""
+	"""Permet la modification des informations personnelles"""
 
-    model = Candidat
-    fields = ['adresse', 'telephone', 'telephone_mobile', 'date_naissance', 'genre']
-    template_name = "candidat_update.html"
-    success_url = "/candidat"
+	model = Candidat
+	fields = ['adresse', 'telephone', 'telephone_mobile', 'date_naissance', 'genre']
+	template_name = "candidat_update.html"
+	success_url = "/candidat"
 
-    @set_candidat
-    def dispatch(self, request, *args, **kwargs):
-        """compare le numero de dossier appelé et celui du candidat connecté"""
-        if kwargs['pk'] != self.candidat.pk:
-            return redirect("/")
-        return super().dispatch(request, *args, **kwargs)
+	@set_candidat
+	def dispatch(self, request, *args, **kwargs):
+		"""compare le numero de dossier appelé et celui du candidat connecté"""
+		if kwargs['pk'] != self.candidat.pk:
+			return redirect("/")
+		return super().dispatch(request, *args, **kwargs)
 
 	# TODO ceci envoie les formulaires dans le contexte, mais ils ne
 	# sont pour l'instant pas traités par POST
