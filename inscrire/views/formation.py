f1c718c2c84f1092e1115adf8715f1a75064b706
Florian Hatat <pykol@hatat.me>
2020-05-24 16:06:01 +0200

    Ébauche de vues formation

---
+++ inscrire/views/formation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+
+# scripsup - Inscription en ligne en CPGE
+# Copyright (c) 2020 Florian Hatat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from django.views.generic import ListView, DetailView
+
+from inscrire.models import Formation
+
+class FormationListView(ListView):
+	model = Formation
+
+class FormationDetailView(DetailView):
+	model = Formation
