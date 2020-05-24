78ad04dc98d26bc93ea8897deac1adc967dd2c5e
Florian Hatat <pykol@hatat.me>
2020-05-24 21:18:17 +0200

    En tests locaux, e-mail dans la console

--- scripsup/settings.py
+++ scripsup/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'inscrire.InscrireUser'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
