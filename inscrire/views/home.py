2c09f6bd3fdec21eb3c6e41e5a36e0bcccb01089
Florian Hatat <pykol@hatat.me>
2020-05-26 17:11:00 +0200

    Ébauche de vue d'accueil direction

--- inscrire/views/home.py
+++ inscrire/views/home.py
@@ -43,7 +43,12 @@ class HomeView(AccessMixin, View):
 			}.get(request.user.role).as_view()(request, *args, **kwargs)
 
 class DirectionHomeView(TemplateView):
-	pass
+	template_name = 'inscrire/home/direction.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['formation_list'] = Formation.objects.all().order_by('etablissement', 'nom')
+		return context
 
 class SecretariatHomeView(TemplateView):
 	pass
