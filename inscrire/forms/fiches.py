85076b2c17bcf006d501e32aa3dc692004a3cb7b
Florian Hatat <pykol@hatat.me>
2020-06-07 10:50:41 +0200

    Formulaire tordu pour ajouter les bulletins scolaires

--- inscrire/forms/fiches.py
+++ inscrire/forms/fiches.py
@@ -25,8 +25,11 @@ de fiche associent la classe de formulaire à utiliser.
 """
 
 from django import forms
+from django.forms.utils import ErrorList
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.utils.functional import cached_property
+from django.utils.safestring import mark_safe
 from dal import autocomplete
 
 from inscrire.models import fiches
@@ -78,7 +81,7 @@ class IdentiteForm(FicheValiderMixin, forms.ModelForm):
 				"rapidement le pays."
 		}
 
-class ScolariteAnterieureForm(FicheValiderMixin, forms.ModelForm):
+class ScolariteAnterieureBaseForm(FicheValiderMixin, forms.ModelForm):
 	prefix = 'fiche-scolariteanterieure'
 	class Meta:
 		model = fiches.FicheScolariteAnterieure
@@ -186,7 +189,7 @@ class ScolariteForm(FicheValiderMixin, forms.ModelForm):
 		return MefOption.objects.filter(
 			formation=self.instance.candidat.voeu_actuel.formation,
 			inscriptions=True)
-	
+
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.fields['options'].queryset = self.options_qs().order_by(
@@ -210,6 +213,191 @@ class ScolariteForm(FicheValiderMixin, forms.ModelForm):
 			raise ValidationError(errors)
 
 		return self.cleaned_data['options']
+
+ScolariteBulletinForm = forms.inlineformset_factory(
+		fiches.FicheScolariteAnterieure, fiches.BulletinScolaire,
+		fields=('classe', 'bulletin'), extra=5, max_num=10,
+		can_delete=True)
+#TODO créer un form dédié pour éditer les bulletins, dérivant de
+#FicheValiderMixin
+
+class ScolariteAnterieureForm:
+	form_classes = [ScolariteAnterieureBaseForm, ScolariteBulletinForm]
+
+	def __init__(self, instance=None, data=None, files=None, auto_id='id_%s', prefix=None,
+			initial=None, error_class=ErrorList, form_kwargs=None):
+		self.instance = instance
+		self.is_bound = data is not None or files is not None
+		self.prefix = prefix or self.get_default_prefix()
+		self.auto_id = auto_id
+		self.data = data or {}
+		self.files = files or {}
+		self.initial = initial
+		self.form_kwargs = form_kwargs or {}
+		self.error_class = error_class
+		self._errors = None
+		self._non_form_errors = None
+
+	def __str__(self):
+		return self.as_table()
+
+	def __iter__(self):
+		"""Yield the forms in the order they should be rendered."""
+		return iter(self.forms)
+
+	def __getitem__(self, index):
+		"""Return the form at the given index, based on the rendering order."""
+		return self.forms[index]
+
+	def __len__(self):
+		return len(self.forms)
+
+	def __bool__(self):
+		"""
+		Return True since all formsets have a management form which is not
+		included in the length.
+		"""
+		return True
+
+	@cached_property
+	def forms(self):
+		"""Instantiate forms at first property access."""
+		return [
+			self._construct_form(i)
+			for i in range(len(self.form_classes))
+		]
+
+	def _construct_form(self, i, **kwargs):
+		"""Instantiate and return the i-th form instance in a formset."""
+		defaults = {
+			'auto_id': self.auto_id,
+			'prefix': self.add_prefix(i),
+			'error_class': self.error_class,
+			'instance': self.instance,
+		}
+		if self.is_bound:
+			defaults['data'] = self.data
+			defaults['files'] = self.files
+		if self.initial and 'initial' not in kwargs:
+			try:
+				defaults['initial'] = self.initial[i]
+			except IndexError:
+				pass
+		defaults.update(kwargs)
+		form = self.form_classes[i](**defaults)
+		return form
+
+	@property
+	def cleaned_data(self):
+		"""
+		Return a list of form.cleaned_data dicts for every form in self.forms.
+		"""
+		if not self.is_valid():
+			raise AttributeError("'%s' object has no attribute 'cleaned_data'" % self.__class__.__name__)
+		return [form.cleaned_data for form in self.forms]
+
+	def get_default_prefix(cls):
+		return 'fiche-scolarite'
+
+	def non_form_errors(self):
+		"""
+		Return an ErrorList of errors that aren't associated with a particular
+		form -- i.e., from formset.clean(). Return an empty ErrorList if there
+		are none.
+		"""
+		if self._non_form_errors is None:
+			self.full_clean()
+		return self._non_form_errors
+
+	@property
+	def errors(self):
+		"""Return a list of form.errors for every form in self.forms."""
+		if self._errors is None:
+			self.full_clean()
+		return self._errors
+
+	def total_error_count(self):
+		"""Return the number of errors across all forms in the formset."""
+		return len(self.non_form_errors()) +\
+			sum(len(form_errors) for form_errors in self.errors)
+
+	def is_valid(self):
+		"""Return True if every form in self.forms is valid."""
+		if not self.is_bound:
+			return False
+		# We loop over every form.errors here rather than short circuiting on the
+		# first failure to make sure validation gets triggered for every form.
+		forms_valid = True
+		# This triggers a full clean.
+		self.errors
+		for form in self:
+			forms_valid &= form.is_valid()
+		return forms_valid and not self.non_form_errors()
+
+	def full_clean(self):
+		"""
+		Clean all of self.data and populate self._errors and
+		self._non_form_errors.
+		"""
+		self._errors = []
+		self._non_form_errors = self.error_class()
+		empty_forms_count = 0
+
+		if not self.is_bound:  # Stop further processing.
+			return
+		for form in self:
+			self._errors.append(form.errors)
+		try:
+			# Give self.clean() a chance to do cross-form validation.
+			self.clean()
+		except ValidationError as e:
+			self._non_form_errors = self.error_class(e.error_list)
+
+	def clean(self):
+		"""
+		Hook for doing any extra formset-wide cleaning after Form.clean() has
+		been called on every form. Any ValidationError raised by this method
+		will not be associated with a particular form; it will be accessible
+		via formset.non_form_errors()
+		"""
+		pass
+
+	def has_changed(self):
+		"""Return True if data in any form differs from initial."""
+		return any(form.has_changed() for form in self)
+
+	def add_prefix(self, index):
+		return '%s-%s' % (self.prefix, index)
+
+	def is_multipart(self):
+		"""
+		Return True if the formset needs to be multipart, i.e. it
+		has FileInput, or False otherwise.
+		"""
+		return all(form.is_multipart() for form in self)
+
+	@property
+	def media(self):
+		return mark_safe(''.join(str(form.media) for form in self))
+
+	def as_table(self):
+		"Return this formset rendered as HTML <tr>s -- excluding the <table></table>."
+		# XXX: there is no semantic division between forms here, there
+		# probably should be. It might make sense to render each form as a
+		# table row with each field as a td.
+		return mark_safe(' '.join(form.as_table() for form in self))
+
+	def as_p(self):
+		"Return this formset rendered as HTML <p>s."
+		return mark_safe(' '.join(form.as_p() for form in self))
+
+	def as_ul(self):
+		"Return this formset rendered as HTML <li>s."
+		return mark_safe(' '.join(form.as_ul() for form in self))
+
+	def save(self, commit=True):
+		for form in self:
+			form.save(commit)
 
 # Dictionnaire qui à chaque modèle de fiche associe le formulaire
 # d'édition qui doit être présenté aux candidats.
