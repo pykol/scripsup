a1d84e83b6c94138f064624262cd1c2f8f1c3c2b
Romain Krust <romain.krust@orange.fr>
2020-06-22 18:18:53 +0200

    En travaux: plusieurs IP exceptionnelles possibles

--- inscrire/intergiciels.py
+++ inscrire/intergiciels.py
@@ -14,10 +14,11 @@ def en_travaux(get_response):
         if en_travaux:
             try:
                 with open(os.path.join(settings.BASE_DIR, 'scripsup/maintenance_mode_ip.txt'), encoding = 'utf-8', newline = '') as f:
-                    IP = f.read().strip()
+                    IPs = [line.strip() for line in f.readlines()]
             except FileNotFoundError:
                 return HttpResponse(en_travaux)
-            if not IP == request.META.get('REMOTE_ADDR'):
+            IP = request.META.get('REMOTE_ADDR')
+            if not IP in IPs :
                 return HttpResponse(en_travaux)
         response = get_response(request)
         return response
